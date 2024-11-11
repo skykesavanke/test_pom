69751ab59beeb6743640f1a0970019d7cc571a92
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-11-11 20:04:50 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,1 +1,9 @@
+// vars/slackNotifier.groovy
+def call(String message, String color = '#36a64f') {
+    slackSend (
+        channel: '#jenkins-slack-notifications',
+        color: color,
+        message: "Success"
+    )
+}
 
