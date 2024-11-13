99748ac9c5a93bc2a87570d0ce444e2f27c7e1e1
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-11-13 22:25:06 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,16 @@
-def call(String buildStatus = 'STARTED') {
-  // Build status of null means success.
-  buildStatus = buildStatus ?: 'SUCCESS'
 
-  def colorCode = '#FF0000'
-  if (buildStatus == 'SUCCESS') {
-    colorCode = '#36A64F'
+
+def sendNotification(String buildStatus) {
+    // Build status logic as per your function
+    buildStatus = buildStatus ?: 'STARTED'
+
+    def colorCode = '#FF0000'
+    if (buildStatus == 'SUCCESS') {
+        colorCode = '#36A64F'
     } else if (buildStatus == 'UNSTABLE') {
-    colorCode = '#FFFF00'
-  }
+        colorCode = '#FFFF00'
+    }
 
-  // Slack message
-  def message = "*${buildStatus}:* Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-
-  slackSend(color: colorCode, message: message, channel: "#${env.SLACK_CHANNEL}")
+    def message = "*${buildStatus}:* Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+    slackSend(color: colorCode, message: message, channel: "#${env.SLACK_CHANNEL}")
 }
